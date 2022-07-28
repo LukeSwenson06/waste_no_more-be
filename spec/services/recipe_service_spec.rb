df1364e7812dad21d53a6f74c8ef627c1afea9ef
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-27 18:44:04 -0600

    Created test for recipe details service call

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipeService' do
-  it "can retrieve data and parses response", :vcr do
+  it "can retrieve data and parses response for ingredients connection", :vcr do
     parsed_json = RecipeService.get_recipe_by_ingredients('milk,+eggs')
 
     expect(parsed_json).to be_a Array
     recipe = parsed_json.first
 
     expect(recipe).to include("id", "title", "image", "imageType", "usedIngredientCount", "missedIngredientCount", "missedIngredients")
+    expect(recipe).to be_a Hash
+  end
+
+  it "can retrieve data and parses response for searching recipe by id" do
+    parsed_json = RecipeService.get_recipe_details(991625)
+
+    expect(parsed_json).to be_a Array
+    recipe = parsed_json.first
+    expect(recipe).to include("id", "title", "sourceUrl")
     expect(recipe).to be_a Hash
   end
 end
