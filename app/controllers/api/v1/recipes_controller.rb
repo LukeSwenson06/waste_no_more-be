a888892c01fd0f67b31d12b9cbdc512a80f43f33
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-29 16:46:10 -0600

    added travis.yml file for CL integration

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipesController < ApplicationController
   def show
     # render json: RecipeSerializer.new(RecipeService.get_recipes(User.find(params[:id]).items))
+    binding.pry
     render json: RecipeService.get_recipes(User.find(params[:id]).items)
   end
 end
